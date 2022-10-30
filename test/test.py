5cbb967ed64354d86af597a4699a353c3d878f64
coffeewithtears <96424644+coffeewithtears@users.noreply.github.com>
2022-10-30 20:39:11 +0600

    Add files via upload

---
+++ test/test.py
@@ -0,0 +1,12 @@
+from nftwebsite import create_app
+
+
+def test_home_page():
+    flask_app = create_app('flask_test.cfg')
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/')
+        assert response.status_code == 200
+        assert b"Welcome to the" in response.data
+        assert b"Flask User Management Example!" in response.data
+        assert b"Need an account?" in response.data
+        assert b"Existing user?" in response.data
